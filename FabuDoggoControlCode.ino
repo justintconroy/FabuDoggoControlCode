69213340e3f0c60727d65dd69e9d78519ccceb9c
Justin T. Conroy <justin@conroy.in>
2018-02-18 08:26:31 -0600

    Setup motors and USB Host board.

--- FabuDoggoControlCode.ino
+++ FabuDoggoControlCode.ino
@@ -1,23 +1,81 @@
 #include <Servo.h>
+#include <XBOXUSB.h>
+#include <SPI.h>
 
-#define ENABLE_UHS_DEBUGGING 1
+//#define ENABLE_UHS_DEBUGGING 1
 
-char serialRead = '0';
+USB Usb;
+XBOXUSB Xbox(&Usb);
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+int mouthPin = 4;
+int mouthOpen = 36;
+int mouthNeutral = 90;
+int mouthClosed = 103;
+Servo mouthServo;
 
-int pos = 0;    // variable to store the servo position
+// White motor is front left.
+int whiteForward = 2;
+int whiteReverse = 3;
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial){}
-  
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo.write(10);
+// Yellow motor is back left.
+int yellowForward = 44;
+int yellowReverse = 45;
+
+// Orange motor is front right.
+int orangeForward = 5;
+int orangeReverse = 6;
+
+// Blue motor is back right.
+int blueForward = 7;
+int blueReverse = 8;
+
+void setup()
+{
+  // Iniitialize the serial port for debugging.
+  Serial.begin(115200);
+#if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+
+  // Initialize the USB Host board.
+  if (Usb.Init() == -1) {
+    Serial.print(F("\r\nOSC did not start"));
+    while (1); //halt
+  }
+  Serial.print(F("\r\nXBOX USB Library Started"));
+
+  // Setup the servo that controls the mouth and
+  // set it to a neutral position.
+  mouthServo.attach(mouthPin);
+  mouthServo.write(mouthNeutral);
+
+  // Initialize all the drive motors and set them to "Stopped".
+  // Motors are labelled by color on the robot. The pin definitions
+  // at the top of this file also indicate which motor each color
+  // maps to.
+  pinMode(whiteForward, OUTPUT);
+  digitalWrite(whiteForward, LOW);
+  pinMode(whiteReverse, OUTPUT);
+  digitalWrite(whiteReverse, LOW);
+
+  pinMode(yellowForward, OUTPUT);
+  digitalWrite(yellowForward, LOW);
+  pinMode(yellowReverse, OUTPUT);
+  pinMode(Reverse, OUTPUT);
+
+  pinMode(orangeForward, OUTPUT);
+  digitalWrite(orangeForward, LOW);
+  pinMode(orangeReverse, OUTPUT);
+  pinMode(Reverse, OUTPUT);
+
+  pinMode(blueForward, OUTPUT);
+  digitalWrite(blueForward, LOW);
+  pinMode(blueReverse, OUTPUT);
+  pinMode(Reverse, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
 
   if (Serial.available() > 0)
   {
@@ -28,19 +86,13 @@ void loop() {
     switch(serialRead)
     {
       case '1':
-        myservo.write(10);
+        mouthServo.write(mouthOpen);
         break;
       case '2':
-        myservo.write(30);
+        mouthServo.write(mouthNeutral);
         break;
       case '3':
-        myservo.write(45);
-        break;
-      case '4':
-        myservo.write(65);
-        break;
-      case '5':
-        myservo.write(80);
+        mouthServo.write(mouthClosed);
         break;
       default:
         break;
