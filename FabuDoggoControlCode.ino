8eed35f772981ccbb4028f9185815570e24a54b0
Justin T. Conroy <justin@conroy.in>
2018-02-18 09:43:48 -0600

    Implements simple drive system.
    Tanks steering where each joystick controls one side of the robot.

--- FabuDoggoControlCode.ino
+++ FabuDoggoControlCode.ino
@@ -1,5 +1,12 @@
 #include <Servo.h>
+#include <limits.h>
+
 #include <XBOXUSB.h>
+
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
 #include <SPI.h>
 
 //#define ENABLE_UHS_DEBUGGING 1
@@ -22,12 +29,25 @@ int yellowForward = 44;
 int yellowReverse = 45;
 
 // Orange motor is front right.
-int orangeForward = 5;
-int orangeReverse = 6;
+int orangeForward = 6;
+int orangeReverse = 5;
 
 // Blue motor is back right.
 int blueForward = 7;
 int blueReverse = 8;
+
+char serialRead;
+
+enum motor
+{
+  white,
+  yellow,
+  orange,
+  blue,
+};
+
+void driveMotor(motor motor, int speed);
+void driveMotor(int motorForwardPin, int motorReversePin, int speed);
 
 void setup()
 {
@@ -61,42 +81,140 @@ void setup()
   pinMode(yellowForward, OUTPUT);
   digitalWrite(yellowForward, LOW);
   pinMode(yellowReverse, OUTPUT);
-  pinMode(Reverse, OUTPUT);
+  pinMode(yellowReverse, OUTPUT);
 
   pinMode(orangeForward, OUTPUT);
   digitalWrite(orangeForward, LOW);
   pinMode(orangeReverse, OUTPUT);
-  pinMode(Reverse, OUTPUT);
+  pinMode(orangeReverse, OUTPUT);
 
   pinMode(blueForward, OUTPUT);
   digitalWrite(blueForward, LOW);
   pinMode(blueReverse, OUTPUT);
-  pinMode(Reverse, OUTPUT);
+  pinMode(blueReverse, OUTPUT);
 }
 
 void loop()
 {
-
-  if (Serial.available() > 0)
+  Usb.Task();
+  if (Xbox.Xbox360Connected)
   {
-    serialRead = Serial.read();
+    // Get joystick values.
+    int leftHatY = Xbox.getAnalogHat(LeftHatY);
+    int rightHatY = Xbox.getAnalogHat(RightHatY);
 
-    Serial.println("Recieved " + serialRead);
+    Serial.print("Left Hat Y: ");
+    Serial.println(leftHatY);
+    Serial.print("Right Hat Y: ");
+    Serial.println(rightHatY);
     
-    switch(serialRead)
+    if (leftHatY > 7500 || leftHatY < -7500)
     {
-      case '1':
-        mouthServo.write(mouthOpen);
-        break;
-      case '2':
-        mouthServo.write(mouthNeutral);
-        break;
-      case '3':
-        mouthServo.write(mouthClosed);
-        break;
-      default:
-        break;
+      int y = map(leftHatY, INT_MIN, INT_MAX, -255, 255);
+      driveMotor(white, y);
+      driveMotor(yellow, y);
     }
+    else
+    {
+      driveMotor(white, 0);
+      driveMotor(yellow, 0);
+    }
+
+    if (rightHatY > 7500 || rightHatY < -7500)
+    {
+      int y = map(rightHatY, INT_MIN, INT_MAX, -255, 255);
+      driveMotor(orange, y);
+      driveMotor(blue, y);
+    }
+    else
+    {
+      driveMotor(orange, 0);
+      driveMotor(blue, 0);
+    }
+  }
+//  if (Serial.available() > 0)
+//  {
+//    serialRead = Serial.read();
+//
+//    Serial.println("Recieved " + serialRead);
+//
+//    switch(serialRead)
+//    {
+//      case '1':
+//        mouthServo.write(mouthOpen);
+//        break;
+//      case '2':
+//        mouthServo.write(mouthNeutral);
+//        break;
+//      case '3':
+//        mouthServo.write(mouthClosed);
+//        break;
+//      default:
+//        break;
+//    }
+//  }
+
+  delay(1);
+}
+
+// Select a motor with the enum parameter,
+// set a speed from -255 to 255 (negative
+// means reverse).
+void driveMotor(motor motor, int speed)
+{
+  switch (motor)
+  {
+    case white:
+      Serial.print("Driving white motor. Speed: ");
+      Serial.println(speed);
+      driveMotor(whiteForward, whiteReverse, speed);
+      break;
+    case yellow:
+      Serial.print("Driving yellow motor. Speed: ");
+      Serial.println(speed);
+      driveMotor(yellowForward, yellowReverse, speed);
+      break;
+    case orange:
+      Serial.print("Driving orange motor. Speed: ");
+      Serial.println(speed);
+      driveMotor(orangeForward, orangeReverse, speed);
+      break;
+    case blue:
+      Serial.print("Driving blue motor. Speed: ");
+      Serial.println(speed);
+      driveMotor(blueForward, blueReverse, speed);
+      break;
+    default:
+      Serial.print("Bad Motor selection: ");
+      Serial.println(motor);
+      break;
+  }
+}
+
+// Select a motor to drive by its pins. Set a speed
+// from -255 to 255 (negative means reverse). This
+// causes a PWM signal corresponding to the speed
+// to be output on either the forward or reverse
+// pin. The other pin will be set to  LOW. If the
+// speed is 0, both pins will be set to LOW.
+void driveMotor(int motorForwardPin, int motorReversePin,int speed)
+{
+  if (speed > 0)
+  {
+    if (speed > 255) speed = 255;
+    digitalWrite(motorReversePin, LOW);
+    analogWrite(motorForwardPin, speed);
+  }
+  else if (speed < 0)
+  {
+    if (speed < -255) speed = -255;
+    digitalWrite(motorForwardPin, LOW);
+    analogWrite(motorReversePin, abs(speed));
+  }
+  else
+  {
+    digitalWrite(motorForwardPin, LOW);
+    digitalWrite(motorReversePin, LOW);
   }
 }
 
